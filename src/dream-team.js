27cb6d18c00f4230adaa48d7d5d3794a06b1bbb5
KirrBrest <lukashchuk.kiryl@yandex.by>
2023-12-25 17:53:13 +0300

    feat: nazvanie_vashego_commita (Mon, Dec 25, 2023 05:53:13 PM)

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if ((typeof members) !== 'object' || members === null || !Array.isArray(members)) {return false} else {
+
+    var arr = [];
+    var allMembers = members.filter(function(el){return typeof(el) === "string"});
+  
+    console.debug(allMembers);
+  
+    for (let i = 0; i < allMembers.length; i++) {
+      let arrMember = [];
+      let mem = allMembers[i].replace(/\s/g,'').split('');
+      arr.push(mem[0].toUpperCase());
+    
+      
+     
+    }
+    console.debug(arr);
+  
+    return arr.sort().join('');
+   }  
+
 }
 
 module.exports = {
